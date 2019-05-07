93a9788d50dfcf742f1a89349a561283c5a7d05d
pankai <273377900@qq.com>
2019-05-07 10:19:07 +0800

    TS

--- js/v1/tpl/common/ajax.ts
+++ js/v1/tpl/common/ajax.ts
@@ -1,48 +1,38 @@
+import axios from 'axios';
 import { buildURL } from 'common/url';
-import * as $ from 'jquery';
-
-declare const window: any;
 $(document).ajaxStart(() => { (window as any).Pace.restart(); });
 
 function countDone(cb: (r: any) => void) {
+    return (r: any) => {
+        cb(r.data);
+    };
+}
+
+function countErrorDone(cb: (r: any) => void) {
     return (r: any) => {
         cb(r);
     };
 }
 
-export function ajaxUploadPost(url: string, data: object | any, done: (result: any) => void, error: (error: any) => void) {
-    $.ajax({
-        type: 'POST',
+export function ajaxPost(url: string, data: object, done: (result: any) => void, error: (error: any) => void) {
+    axios({
+        method: 'POST',
         url,
-        data,
-        cache: false,
-        contentType: false,
-        processData: false,
-    }).done(countDone(done)).fail(countDone((err) => {
-        alert(`网络不稳定，接口请求超时，请稍后重试 !!!`);
-        error(err);
-    }));
-}
-
-export function ajaxPost(url: string, data: object | any, done: (result: any) => void, error: (error: any) => void) {
-    $.ajax({
-        type: 'POST',
-        url,
-        dataType: 'json',
-        contentType: 'application/json; charset=utf-8',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
         data: JSON.stringify(data),
-    }).done(countDone(done)).fail(countDone((err) => {
-        alert(`网络不稳定，接口请求超时，请稍后重试 !!!`);
+    }).then(countDone(done)).catch(countErrorDone((err) => {
+        console.warn(url, err);
         error(err);
     }));
 }
 
 export function ajaxGet(url: string, done: (result: any) => void, error: (error: any) => void) {
-    $.ajax({
-        type: 'GET',
-        data: { token: window.app && window.app.token ? window.app.token : undefined },
+    axios({
+        method: 'GET',
         url,
-    }).done(countDone(done)).fail(countDone(error));
+    }).then(countDone(done)).catch(countErrorDone(error));
 }
 
 export function ajaxPostFormData(
@@ -51,22 +41,14 @@ export function ajaxPostFormData(
     done: (result: any) => void,
     error: (error: any) => void) {
 
-    $.ajax({
-        type: 'POST',
+    axios({
+        method: 'POST',
         url,
-        contentType: false,
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
         data,
-        processData: false,
-    }).done(countDone(done)).fail(countDone((err) => {
-        alert(`网络不稳定，接口请求超时，请稍后重试 !!!`);
-        error(err);
-    }));
-}
-
-export function uploadPostPromise(url: string, data: object): Promise<any> {
-    return new Promise((resolve, reject) => {
-        ajaxUploadPost(url, data, resolve, reject);
-    });
+    }).then(countDone(done)).catch(countErrorDone(error));
 }
 
 export function postPromise(url: string, data: object): Promise<any> {
