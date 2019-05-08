31f32f058cb391f25b6a83ebab47fd72bdc382b9
pankai <273377900@qq.com>
2019-05-08 10:01:39 +0800

    TS

--- js/v1/tpl/mobile/app/bill/index.tsx
+++ js/v1/tpl/mobile/app/bill/index.tsx
@@ -1,4 +1,4 @@
-import { AuthProvider } from 'common/auth';
+import { AuthProvider, setDefaultLoginURL } from 'common/auth';
 import * as React from 'react';
 import { render } from 'react-dom';
 import { routes } from './routes';
@@ -6,6 +6,8 @@ import { routes } from './routes';
 import './common/apply_theme.less';
 import { AppStateProvider } from './common/appStateStore';
 import { SearchToObject } from './common/publicData';
+
+setDefaultLoginURL('/bill/user/login');
 
 declare const window: any;
 
