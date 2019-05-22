b56b7296ec9983018a2a9c93c3b84444a5fbba9e
pankai <273377900@qq.com>
2019-05-22 20:34:18 +0800

    TS

--- js/v1/tpl/mobile/app/apply/page/apply/home.tsx
+++ js/v1/tpl/mobile/app/apply/page/apply/home.tsx
@@ -1,5 +1,8 @@
 import { Steps } from 'common/antd/mobile/steps';
+import { Querier } from 'common/component/restFull';
 import { Radium } from 'common/radium';
+import * as _ from 'lodash';
+import { autorun, observable, reaction } from 'mobx';
 import { observer } from 'mobx-react';
 import * as React from 'react';
 
@@ -8,8 +11,40 @@ const Step = Steps.Step;
 @Radium
 @observer
 export class Home extends React.Component<{}, {}> {
+    private query: Querier<any, any> = new Querier(null);
+    private disposers: Array<() => void> = [];
+
+    @observable private loading: boolean = false;
+    @observable private resultData: any;
+
     constructor(props: any) {
         super(props);
+    }
+
+    componentWillUnmount() {
+        this.disposers.forEach(f => f());
+        this.disposers = [];
+    }
+
+    componentDidMount() {
+        this.getAuth();
+    }
+
+    getAuth() {
+        this.query.setReq({
+            url: '/api/mobile/authdata',
+            method: 'get',
+        });
+
+        this.disposers.push(autorun(() => {
+            this.loading = this.query.refreshing;
+        }));
+
+        this.disposers.push(reaction(() => {
+            return (_.get(this.query.result, 'result.data') as any) || [];
+        }, searchData => {
+            this.resultData = searchData;
+        }));
     }
 
     render() {
