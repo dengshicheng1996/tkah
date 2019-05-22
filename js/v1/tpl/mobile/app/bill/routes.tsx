7e37e07b2d32f705727b379477fa87c776f7a63e
pankai <273377900@qq.com>
2019-05-22 19:58:14 +0800

    TS

--- js/v1/tpl/mobile/app/bill/routes.tsx
+++ js/v1/tpl/mobile/app/bill/routes.tsx
@@ -10,7 +10,7 @@ import { routes as pageRoutes } from './page/routes';
 export const routes = (
     <Router>
         <Switch>
-            <Route path='/' component={() => pageRoutes} />
+            <Route path='/bill' component={() => pageRoutes} />
         </Switch>
     </Router>
 );
