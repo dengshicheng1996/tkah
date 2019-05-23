a4ff6ea3b5e1418bc9a171e7248ca7d5aeb35fe7
pankai <273377900@qq.com>
2019-05-24 00:21:20 +0800

    TS

--- js/v1/tpl/common/qiniuUpload.ts
+++ js/v1/tpl/common/qiniuUpload.ts
@@ -1,21 +1,22 @@
 const qiniu = require('qiniu-js');
 
-const upload = (data: any) => {
+const upload = (data: any, next?: (res: any) => void, onError?: (err: any) => void, complete?: (res: any) => void) => {
     const putExtra: any = {
         fname: this.token.substring(0, 5) + new Date().getTime(),
         params: {},
     };
     const key = this.token.substring(0, 5) + new Date().getTime();
     const obser = qiniu.upload(data.file, key, this.token, putExtra, {});
+
     obser.subscribe({
         next: (res: any) => {
-            this.props.next && this.props.next(res);
+            next && next(res);
         },
         error: (err: any) => {
-            this.props.onError && this.props.onError(err);
+            onError && onError(err);
         },
         complete: (res: any) => {
-            this.props.complete && this.props.complete('http://imgcdn.alphae.cn/' + res.key);
+            complete && complete('http://imgcdn.alphae.cn/' + res.key);
         },
     });
 };
