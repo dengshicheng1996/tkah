a5f8f88113234065378d26d6be735530e2295e7c
pankai <273377900@qq.com>
2019-05-23 21:03:46 +0800

    TS

--- js/v1/tpl/mobile/app/apply/index.tsx
+++ js/v1/tpl/mobile/app/apply/index.tsx
@@ -11,13 +11,13 @@ import { AppStateProvider } from 'mobile/common/appStateStore';
 
 declare const window: any;
 
-// tslint:disable-next-line:max-line-length
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9uZXdzLmxtcy5jb21cL2FwaVwvbW9iaWxlXC9sb2dpbiIsImlhdCI6MTU1ODQ1ODg0OSwiZXhwIjoxNTY2MjM0ODQ5LCJuYmYiOjE1NTg0NTg4NDksImp0aSI6IkVXNVlxekI2bXAwajhhbDEiLCJzdWIiOjE5LCJwcnYiOiI1OTYzZDYxMDEwOTI5M2U2Nzc4ZGYyYjZiMmVhNzI2Yjg1MTQxN2Q1In0.Qov8lCFcYmz8ycWyIc1HyMboPtnwGadsgcCf01y3daw';
-$.cookie('token', token, { path: '/' });
-
 if (SearchToObject(window.location.search)['token']) {
     $.cookie('token', SearchToObject(window.location.search)['token'], { path: '/' });
     window.location.href = window.location.pathname;
+} else {
+    // tslint:disable-next-line:max-line-length
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9uZXdzLmxtcy5jb21cL2FwaVwvbW9iaWxlXC9sb2dpbiIsImlhdCI6MTU1ODQ1ODg0OSwiZXhwIjoxNTY2MjM0ODQ5LCJuYmYiOjE1NTg0NTg4NDksImp0aSI6IkVXNVlxekI2bXAwajhhbDEiLCJzdWIiOjE5LCJwcnYiOiI1OTYzZDYxMDEwOTI5M2U2Nzc4ZGYyYjZiMmVhNzI2Yjg1MTQxN2Q1In0.Qov8lCFcYmz8ycWyIc1HyMboPtnwGadsgcCf01y3daw';
+    $.cookie('token', token, { path: '/' });
 }
 
 window.navbar = {};
