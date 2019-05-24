88e20ad8f16b6ed6f5b94d7d3630884d38bd4824
pankai <273377900@qq.com>
2019-05-24 15:20:53 +0800

    TS

--- js/v1/tpl/mobile/app/apply/page/apply/home.tsx
+++ js/v1/tpl/mobile/app/apply/page/apply/home.tsx
@@ -129,7 +129,9 @@ class HomeView extends React.Component<RouteComponentProps<any> & WithAppState, 
         );
     }
 
-    private gotoPage = () => {
+    private gotoPage = (ev: any) => {
+        ev.preventDefault();
+        ev.stopPropagation();
         console.log(0);
         const stepInfo = this.props.data.stepInfo.steps[this.props.data.stepInfo.stepNumber];
         console.log(toJS(stepInfo));
