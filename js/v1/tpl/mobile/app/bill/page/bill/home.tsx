1f4d601e90ed7c6a2b55bafff8499cf51db8e88d
pankai <273377900@qq.com>
2019-05-27 14:36:37 +0800

    BILL

--- js/v1/tpl/mobile/app/bill/page/bill/home.tsx
+++ js/v1/tpl/mobile/app/bill/page/bill/home.tsx
@@ -1,13 +1,49 @@
+import { AppFn, IsAppPlatform, NavBarBack, NavBarTitle } from 'common/app';
+import { Querier } from 'common/component/restFull';
+import { Radium } from 'common/radium';
+import * as _ from 'lodash';
+import { withAppState, WithAppState } from 'mobile/common/appStateStore';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 import * as React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
-export class Home extends React.Component<{}, {}> {
+@Radium
+@observer
+class HomeView extends React.Component<RouteComponentProps<any> & WithAppState, {}> {
+    private query: Querier<any, any> = new Querier(null);
+    private disposers: Array<() => void> = [];
+
+    @observable private loading: boolean = false;
+    @observable private resultData: any = [];
+    @observable private stepNumber: number = -1;
+    @observable private animating: boolean = false;
+
     constructor(props: any) {
         super(props);
-    }
-    render() {
-        return (
-            <div>bill</div>
-        );
+        NavBarBack(() => {
+            if (IsAppPlatform()) {
+                AppFn.actionFinish();
+            } else {
+                window.history.back();
+            }
+        });
+        NavBarTitle('账单', () => {
+            this.props.data.pageTitle = '账单';
+        });
     }
 
+    componentWillUnmount() {
+        this.disposers.forEach(f => f());
+        this.disposers = [];
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
 }
+
+export const Home = withRouter(withAppState(HomeView));
