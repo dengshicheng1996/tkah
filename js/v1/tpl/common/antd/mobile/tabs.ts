b9a0ea0a1f912bb9024192c4656be30d866f89b4
pankai <273377900@qq.com>
2019-05-27 15:25:13 +0800

    TS

---
+++ js/v1/tpl/common/antd/mobile/tabs.ts
@@ -0,0 +1,4 @@
+import C from 'antd-mobile/lib/tabs';
+import 'antd-mobile/lib/tabs/style/css';
+import 'common/antd/mobile/default-style.css';
+export const Tabs: typeof C = require('antd-mobile/lib/tabs').default;
