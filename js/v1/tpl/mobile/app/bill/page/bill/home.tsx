5708bf173bc924c73031c221919824ebde5acd4f
pankai <273377900@qq.com>
2019-05-29 23:51:33 +0800

    TS

--- js/v1/tpl/mobile/app/bill/page/bill/home.tsx
+++ js/v1/tpl/mobile/app/bill/page/bill/home.tsx
@@ -1,3 +1,4 @@
+import { Card } from 'common/antd/mobile/card';
 import { Flex } from 'common/antd/mobile/flex';
 import { NoticeBar } from 'common/antd/mobile/notice-bar';
 import { Tabs } from 'common/antd/mobile/tabs';
@@ -13,7 +14,6 @@ import moment = require('moment');
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { Sticky, StickyContainer } from 'react-sticky';
-import { style } from 'typestyle';
 import { ModalBank } from './modal/bank';
 import { ModalInfo } from './modal/info';
 
@@ -140,32 +140,40 @@ class HomeView extends React.Component<RouteComponentProps<any> & WithAppState, 
                         { title: '当前待还' },
                         { title: '剩余待还' },
                     ]}
-                        initialPage={0}
+                        initialPage={1}
                         renderTabBar={(props: any) => (
                             <Sticky>
                                 {(p) => <div style={{ ...p.style, zIndex: 1 }}><Tabs.DefaultTabBar {...props} /></div>}
                             </Sticky>
                         )}>
-                        {
-                            this.currentBillData.map((r, i) => {
-                                const order = [];
-                                if (r.bill) {
-                                    order.push(
-                                        <BillServiceFeeView key='bill' type='bill' info={r.bill} overdue_status={r.overdue_status} />,
-                                    );
-                                }
-                                if (r.service_fee) {
-                                    order.push(
-                                        <BillServiceFeeView key='service_fee' type='service_fee' info={r.service_fee} />,
-                                    );
-                                }
-                                return (
-                                    <div key={i}>{order}</div>
-                                );
-                            })
-                        }
                         <div>
-                            Content of third tab
+                            {
+                                this.currentBillData.map((r: any, i: number) => {
+                                    const order = [];
+                                    if (r.bill) {
+                                        order.push(
+                                            <CurrentBill key='bill' type='bill' info={r.bill} overdue_status={r.overdue_status} />,
+                                        );
+                                    }
+                                    if (r.service_fee) {
+                                        order.push(
+                                            <CurrentBill key='service_fee' type='service_fee' info={r.service_fee} />,
+                                        );
+                                    }
+                                    return (
+                                        <div key={i}>{order}</div>
+                                    );
+                                })
+                            }
+                        </div>
+                        <div>
+                            {
+                                this.lastBillData.map((r: any, i: number) => {
+                                    return (
+                                        <LastBill key={i} info={r} />
+                                    );
+                                })
+                            }
                         </div>
                     </Tabs>
                 </StickyContainer>
@@ -177,8 +185,8 @@ class HomeView extends React.Component<RouteComponentProps<any> & WithAppState, 
 
 @Radium
 @observer
-class BillServiceFeeView extends React.Component<{ info: any, type: string, overdue_status?: number }, {}> {
-    private description = {
+class CurrentBill extends React.Component<{ info: any, type: string, overdue_status?: number }, {}> {
+    private description: { [key: number]: JSX.Element } = {
         1: (<span>我没在<span style={{ color: '#E55800' }}>还款日当日</span>开始自动扣款，请确保储蓄卡资金充足，或主动还款。</span>),
         2: (<span>您的账单<span style={{ color: '#E55800' }}>已逾期</span>，请尽快还款，否则将<span style={{ color: '#E55800' }}>产生罚息</span>同时将影响您的<span style={{ color: '#E55800' }}>个人征信</span>。</span>),
     };
@@ -270,6 +278,88 @@ class BillServiceFeeView extends React.Component<{ info: any, type: string, over
     private showDetail(info: any) {
         this.detail = info;
         this.detailModal = true;
+    }
+}
+
+@Radium
+@observer
+class LastBill extends React.Component<{ info: any }, {}> {
+
+    render() {
+        const { info } = this.props;
+        return (
+            <div style={{ paddingTop: '20px' }}>
+                <Card>
+                    <Card.Header
+                        title='This is title'
+                        thumb='https://gw.alipayobjects.com/zos/rmsportal/MRhHctKOineMbKAZslML.jpg'
+                        extra={<span>this is extra</span>}
+                    />
+                    <Card.Body>
+                        <div>This is content of `Card`</div>
+                    </Card.Body>
+                    <Card.Footer content='footer content' extra={<div>extra footer content</div>} />
+                </Card>
+                {/* <div style={{
+                    background: 'rgba(255,255,255,1)',
+                    boxShadow: '0px 1px 5px 0px rgba(171,171,171,0.2)',
+                    borderRadius: '10px',
+                    padding: '20px',
+                }}>
+                    <div style={{ color: 'rgba(153,153,153,1)', textAlign: 'center', fontSize: '14px' }}>
+                        {type === 'bill' ? `${moment(info.should_repayment_date_text).format('YYYY年MM月DD日')}应还（元）` : '手续费'}
+                    </div>
+                    <div style={{ color: '#E55800', textAlign: 'center', fontSize: '50px', marginTop: '15px' }}>
+                        {type === 'bill' ? info.period_amount : info.service_chargea_amount}
+                        <span style={{
+                            padding: '5px',
+                            fontSize: '12px',
+                            background: 'rgba(255,103,74,1)',
+                            color: '#fff',
+                            verticalAlign: 'text-top',
+                            borderRadius: '12px 12px 12px 0px',
+                        }} onClick={() => { this.showDetail(info); }}>详情</span>
+                    </div>
+                    <div style={{ color: 'rgba(153,153,153,1)', textAlign: 'center', fontSize: '14px', marginTop: '22px' }}>
+                        {type === 'bill' ? this.description[overdue_status] : this.serviceFee}
+                    </div>
+                    <Flex style={{ marginTop: '30px' }}>
+                        {
+                            info.allow_extend === 1 ? (
+                                <Flex.Item>
+                                    <div style={{
+                                        background: 'linear-gradient(131deg,rgba(72,131,250,1) 0%,rgba(98,54,255,1) 100%)',
+                                        boxShadow: '0px 1px 5px 0px rgba(171,171,171,0.6)',
+                                        borderRadius: '22px',
+                                        fontSize: '15px',
+                                        fontWeight: 500,
+                                        color: '#fff',
+                                        lineHeight: '21px',
+                                        textAlign: 'center',
+                                        padding: '12px 0',
+                                        margin: '0 10px',
+                                    }}>申请展期</div>
+                                </Flex.Item>
+                            ) : null
+                        }
+                        <Flex.Item>
+                            <div style={{
+                                background: 'linear-gradient(119deg,rgba(252,155,4,1) 0%,rgba(247,80,15,1) 100%)',
+                                boxShadow: '0px 1px 5px 0px rgba(171,171,171,0.6)',
+                                borderRadius: '22px',
+                                fontSize: '15px',
+                                fontWeight: 500,
+                                color: '#fff',
+                                lineHeight: '21px',
+                                textAlign: 'center',
+                                padding: '12px 0',
+                                margin: '0 10px',
+                            }}>主动还款</div>
+                        </Flex.Item>
+                    </Flex>
+                </div> */}
+            </div>
+        );
     }
 }
 
