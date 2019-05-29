997609484237c028bd20dd1d64c185faaefe1503
pankai <273377900@qq.com>
2019-05-29 19:52:17 +0800

    TS

--- js/v1/tpl/mobile/app/apply/page/apply/modules/operator.tsx
+++ js/v1/tpl/mobile/app/apply/page/apply/modules/operator.tsx
@@ -127,7 +127,7 @@ export class OperatorView extends React.Component<RouteComponentProps<any> & Wit
         const { modules, moduleNumber } = this.props.data.moduleInfo;
         console.log(toJS(this.props.data.moduleInfo));
         if (moduleNumber === modules.length - 1) {
-            console.log(toJS(this.props.data.stepInfo.steps));
+            console.log(toJS(this.props.data.stepInfo));
             const stepInfo = untracked(() => {
                 this.props.data.stepInfo.stepNumber++;
                 return this.props.data.stepInfo.steps[this.props.data.stepInfo.stepNumber];
