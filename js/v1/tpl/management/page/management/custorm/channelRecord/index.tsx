be2ac720216cb39c74b68b49c87937d2a6820fdd
Jarvan <liushileijarvan@gmail.com>
2019-05-31 16:08:48 +0800

    合并

--- js/v1/tpl/management/page/management/custorm/channelRecord/index.tsx
+++ js/v1/tpl/management/page/management/custorm/channelRecord/index.tsx
@@ -15,34 +15,42 @@ import Title from '../../../../common/TitleComponent';
 @observer
 class Account extends React.Component<any, any> {
     private tableRef: TableList;
+    @observable private channel: any[] = [];
     constructor(props: any) {
         super(props);
     }
+    beforeRequest(data: any) {
+        const json: any = data;
+        if (data.time) {
+            json.start_date = data.time[0].format('YYYY-MM-DD');
+            json.end_date = data.time[1].format('YYYY-MM-DD');
+            delete json.time;
+        }
+        return json;
+    }
     componentDidMount() {
-        // mutate<{}, any>({
-        //     url: '/api/admin/account/allroles',
-        //     method: 'get',
-        //     // variables: json,
-        // }).then(r => {
-        //     if (r.status_code === 200) {
-        //         this.roleInfo = r.data;
-        //     }
-        // });
+        mutate<{}, any>({
+            url: '/api/admin/account/allroles',
+            method: 'get',
+        }).then(r => {
+            if (r.status_code === 200) {
+                this.channel = r.data;
+            }
+        });
     }
 
     render() {
         const columns = [
-            { title: '手机号', key: 'remark', dataIndex: 'remark' },
-            { title: '姓名', key: 'mobile', dataIndex: 'mobile' },
-            { title: '渠道名称', key: 'role_name', dataIndex: 'role_name' },
-            { title: '状态', key: 'status', dataIndex: 'status', render(status: number | string) { return +status === 1 ? '已启用' : '已禁用'; } },
+            { title: '手机号', key: 'phone', dataIndex: 'phone' },
+            { title: '姓名', key: 'id_name', dataIndex: 'id_name' },
+            { title: '渠道名称', key: 'channel_name', dataIndex: 'channel_name' },
             { title: '访问时间', key: 'created_at', dataIndex: 'created_at' },
         ];
         const search: BaseFormItem[] = [
-            { itemProps: { label: '客户姓名' }, typeComponentProps: { placeholder: '客户姓名' }, key: 'name', type: 'input' },
+            { itemProps: { label: '客户姓名' }, typeComponentProps: { placeholder: '客户姓名' }, key: 'id_name', type: 'input' },
             { itemProps: { label: '客户手机号' }, typeComponentProps: { placeholder: '客户手机号' }, key: 'phone', type: 'input' },
             {
-                itemProps: { label: '渠道名称', hasFeedback: false }, key: 'channel', type: 'select', options: [],
+                itemProps: { label: '渠道名称', hasFeedback: false }, typeComponentProps: { showSearch: true }, key: 'channel_id', type: 'select', options: this.channel,
             },
             { itemProps: { label: '访问时间' }, key: 'time', type: 'rangePicker' },
         ];
@@ -50,9 +58,10 @@ class Account extends React.Component<any, any> {
             <Title>
                 <SearchTable
                     ref={(ref) => { this.tableRef = ref; }}
-                    requestUrl='/api/admin/account/users'
+                    requestUrl='/api/admin/customer/channel/visit/log'
                     tableProps={{ columns }}
                     query={{ search }}
+                    beforeRequest={(data) => this.beforeRequest(data)}
                 />
             </Title>
         );
