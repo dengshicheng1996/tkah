25645312028acc6dc36e2edf11fa964d193ddde9
pankai <273377900@qq.com>
2019-06-05 01:58:46 +0800

    TS

--- js/v1/tpl/mobile/app/withdraw/page/withdraw/home.tsx
+++ js/v1/tpl/mobile/app/withdraw/page/withdraw/home.tsx
@@ -303,9 +303,7 @@ class HomeView extends React.Component<RouteComponentProps<any> & WithAppState, 
         }).then(r => {
             if (r.status_code === 200) {
                 Toast.info('操作成功', 0.5, () => {
-                    if (this.props.location.state.callBackUrl) {
-                        AppFn.actionFinish();
-                    }
+                    AppFn.actionFinish();
                 });
                 return;
             }
