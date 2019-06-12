22059381b0796d11beda85db5ee3c7c38ae38f47
Jarvan <liushileijarvan@gmail.com>
2019-06-12 19:56:32 +0800

    Merge branch 'master' of git.yunbeifenqi.com:MR/front-end into Jarvan
    # Conflicts:
    #	js/v1/tpl/management/page/management/credit/withdraw/detail.tsx

--- js/v1/tpl/management/page/management/credit/withdraw/detail.tsx
+++ js/v1/tpl/management/page/management/credit/withdraw/detail.tsx
@@ -363,7 +363,6 @@ export default class Audit extends React.Component<{}, any> {
             <CardClass title='费用和账单' content={order} />,
             loan_order_fee ?
             <Condition
-                // id={id ? loan_order.id : ''}
                 onOk={() => this.getDetail()}
                 serviceChargeId={loan_order_fee.service_charge_id}
                 customerId={customer.id}
