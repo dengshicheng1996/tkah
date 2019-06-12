ce143dc53a9696e131b17a63729d13aec3583491
pankai <273377900@qq.com>
2019-06-12 11:35:48 +0800

    TS

--- js/v1/tpl/build/fuse-defs.ts
+++ js/v1/tpl/build/fuse-defs.ts
@@ -2,13 +2,13 @@ import { NewSpec } from './fuselib';
 
 const spec = NewSpec();
 
-spec.AddSPA('mobile/app/httpStatus404');
-spec.AddSPA('mobile/app/httpStatus500');
-spec.AddSPA('mobile/app/promotion');
-spec.AddSPA('mobile/app/apply');
-spec.AddSPA('mobile/app/bill');
-spec.AddSPA('mobile/app/withdraw');
+// spec.AddSPA('mobile/app/httpStatus404');
+// spec.AddSPA('mobile/app/httpStatus500');
+// spec.AddSPA('mobile/app/promotion');
+// spec.AddSPA('mobile/app/apply');
+// spec.AddSPA('mobile/app/bill');
+// spec.AddSPA('mobile/app/withdraw');
 spec.AddSPA('management');
-spec.AddSPA('operatePlat');
+// spec.AddSPA('operatePlat');
 
 spec.Run();
