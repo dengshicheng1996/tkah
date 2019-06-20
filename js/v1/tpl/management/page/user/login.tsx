9765439949a49178f66ce908c05e4fe278ad0e60
Jarvan <liushileijarvan@gmail.com>
2019-06-20 15:35:29 +0800

    Merge branch 'master' of git.yunbeifenqi.com:MR/front-end into Jarvan
    # Conflicts:
    #	js/v1/tpl/management/page/management/credit/withdraw/detail.tsx

--- js/v1/tpl/management/page/user/login.tsx
+++ js/v1/tpl/management/page/user/login.tsx
@@ -87,6 +87,7 @@ class LoginView extends React.Component<RouteComponentProps<any> & WithAuth & Lo
                         return {};
                     });
                     if (res.status_code === 200) {
+                        message.success('修改成功')
                         $.cookie('token', res.data.token, { path: '/' });
                         this.props.auth.status.state = 'user';
                         this.props.history.push('/management/home');
