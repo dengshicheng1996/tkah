0f0ddc76f22d810d9d7a77419c3d8d5b5add732d
Jarvan <liushileijarvan@gmail.com>
2019-06-28 16:01:22 +0800

    Merge branch 'develop' of git.yunbeifenqi.com:MR/front-end into Jarvan
    # Conflicts:
    #	js/v1/yarn.lock

--- js/v1/tpl/management/page/management/noPermission.tsx
+++ js/v1/tpl/management/page/management/noPermission.tsx
@@ -1,22 +1,25 @@
 import { staticImgURL } from 'common/staticURL';
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
+import {withAppState} from '../../common/appStateStore';
+import Title from '../../common/TitleComponent';
 class Home extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
+        props.data.appState.panes = [];
         setTimeout(() => {
             props.history.push('/management/home');
         }, 3000);
     }
     render() {
         return (
-            <div>
-                <div>
+            <Title>
+                <div style={{ textAlign: 'center', padding: '200px 0 300px 0' }}>
                     <img src={staticImgURL('noPermission.png')} />
                 </div>
-            </div>
+            </Title>
         );
     }
 
 }
-export default withRouter(Home);
+export default withRouter(withAppState(Home));
--- js/v1/tpl/management/routes.tsx
+++ js/v1/tpl/management/routes.tsx
@@ -6,14 +6,12 @@ import {
     Switch,
 } from 'react-router-dom';
 import { LayoutBase } from './page/layoutBase';
-import noPermission from './page/noPermission';
 import { UserRouter } from './page/user/routes';
 
 export const routes = (
     <Router>
         <Switch>
             <Route path='/management/user' component={() => UserRouter} />
-            <Route path='/management/noPermission' component={noPermission} />
             <Route path='/management' component={LayoutBase} />
             <Route path='/' component={LayoutBase} />
         </Switch>
