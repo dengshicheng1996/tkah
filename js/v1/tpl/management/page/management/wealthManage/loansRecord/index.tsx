7a8981a049c7a9dddec2175fee93e511b26f7a74
Jarvan <liushileijarvan@gmail.com>
2019-07-26 16:48:19 +0800

    合并代码

--- js/v1/tpl/management/page/management/wealthManage/loansRecord/index.tsx
+++ js/v1/tpl/management/page/management/wealthManage/loansRecord/index.tsx
@@ -32,7 +32,7 @@ class Account extends React.Component<any, any> {
             { title: '贷款编号', key: 'loan_no', dataIndex: 'loan_no', fixed: true, width: '200px'},
             { title: '客户姓名', key: 'name', dataIndex: 'name', fixed: true, width: '100px',
                 render: (data: string, record: any) => {
-                    return jurisdiction.indexOf(69) > -1 ? <Link to={'management/custorm/list/' + record.customer_id}>{data}</Link> : data;
+                    return jurisdiction.indexOf(69) > -1 ? <Link to={'/management/custorm/list/' + record.customer_id}>{data}</Link> : data;
                 },
             },
             { title: '放款时间', key: 'pay_time_text', dataIndex: 'pay_time_text' },
